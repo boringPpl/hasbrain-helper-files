9e266bdaefaa3cedc8f6636c16809ba0f97af7dc
chuonghuynh <minhchuong.itus@gmail.com>
2018-11-30 10:43:32 +0700

    Add util

--- cat-non-cat-logistic-regression/utility.py
+++ cat-non-cat-logistic-regression/utility.py
@@ -47,3 +47,106 @@ def sigmoid(z):
     """
 
     return 1/(1+np.exp(-z))
+
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+
+    m = X.shape[1]
+    # FORWARD PROPAGATION (FROM X TO COST)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A = sigmoid(np.dot(w.T,X)+b) # compute activation
+    cost = -1/m * np.sum(Y * np.log(A) + (1-Y) * (np.log(1-A))) # compute cost
+    ### END CODE HERE ###
+
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dz= (1/m)*(A - Y)
+    dw = np.dot(X,dz.T) # gradient of w
+    db = np.sum(dz) # gradient of bias
+    ### END CODE HERE ###
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+
+    Tips:
+    You basically need to write down two steps and iterate through them:
+        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+        2) Update the parameters using gradient descent rule for w and b.
+    """
+
+    costs = []
+
+    for i in range(num_iterations):
+
+
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        ### START CODE HERE ###
+        grads, cost = propagate(w, b, X, Y)
+        ### END CODE HERE ###
+
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+
+        # update rule (≈ 2 lines of code)
+        ### START CODE HERE ###
+        w -= (learning_rate*dw)
+        b -= (learning_rate*db)
+        ### END CODE HERE ###
+
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+
+    params = {"w": w,
+              "b": b}
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
