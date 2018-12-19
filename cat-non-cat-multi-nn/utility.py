ec71c225894065fe60aa3c8a9b39b5522d88fac0
chuonghuynh <minhchuong.itus@gmail.com>
2018-12-19 08:06:11 +0700

    Compiled code

--- cat-non-cat-multi-nn/utility.py
+++ cat-non-cat-multi-nn/utility.py
@@ -577,17 +577,17 @@ def update_parameters(parameters, grads, learning_rate):
     ### END CODE HERE ###
     return parameters
 
-def load_dataset():
+def load_dataset(dir_path):
     '''
     Load dataset from h5 file to numpy array
     Returns:
     - A tuple: (training data array, training label array, test data array, test label array, number of class)
     '''
-    train_dataset = h5py.File('input/catnnoncat/cat_n_noncat/train_catvnoncat.h5', "r")
+    train_dataset = h5py.File('/train_catvnoncat.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('input/catnnoncat/cat_n_noncat/test_catvnoncat.h5', "r")
+    test_dataset = h5py.File(dir_path+'/test_catvnoncat.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
@@ -598,13 +598,13 @@ def load_dataset():
 
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-def load_standardlized_dataset():
+def load_standardlized_dataset(dir_path):
     '''
     Load the dataset and preprocess it
     Returns:
     - A tuple: (standardlized training data array, training label array, standardlized test data array, test label array, number of class)
     '''
-    train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+    train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset(dir_path)
     train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
     test_set_x_flatten =  test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
     train_set_x = train_set_x_flatten/255.
