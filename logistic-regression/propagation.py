76dd743e103ef539de9931a1cd0583e5f1280df2
chuonghuynh <minhchuong.itus@gmail.com>
2018-12-24 11:56:43 +0700

    Compiled code

---
+++ logistic-regression/propagation.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    return 1/(1+np.exp(-z))
+
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+
+    m = X.shape[1]
+    # FORWARD PROPAGATION (FROM X TO COST)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A = sigmoid(np.dot(w.T,X)+b) # compute activation
+    cost = -1/m * np.sum(Y * np.log(A) + (1-Y) * (np.log(1-A))) # compute cost
+    ### END CODE HERE ###
+
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dz= (1/m)*(A - Y)
+    dw = np.dot(X,dz.T) # gradient of w
+    db = np.sum(dz) # gradient of bias
+    ### END CODE HERE ###
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
