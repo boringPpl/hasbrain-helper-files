f6e80e07a042e442202b53f4af57d9b5e214a5db
chuonghuynh <minhchuong.itus@gmail.com>
2018-12-28 16:46:48 +0700

    Compiled code

---
+++ neural-network/forward.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def forward_propagation_test_case():
+    np.random.seed(1)
+    X_assess = np.random.randn(3, 2)
+
+    parameters = {'W1': np.array([[-0.00416758, -0.00056267],
+        [-0.02136196,  0.01640271],
+        [-0.01793436, -0.00841747],
+        [ 0.00502881, -0.01245288]]),
+     'W2': np.array([[-0.01057952, -0.00909008,  0.00551454,  0.02292208]]),
+     'b1': np.array([[ 0.],
+        [ 0.],
+        [ 0.],
+        [ 0.]]),
+     'b2': np.array([[ 0.]])}
+
+    return X_assess, parameters
