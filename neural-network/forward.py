366095c083b78ca1d09deb9db20d6eb58fb68268
chuonghuynh <minhchuong.itus@gmail.com>
2018-12-29 18:04:10 +0700

    Compiled code

--- neural-network/forward.py
+++ neural-network/forward.py
@@ -1,8 +1,33 @@
 import numpy as np
 
+def sigmoid(x):
+    s = 1/(1+np.exp(-x))
+    return s
+
+def forward_propagation(X, parameters):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    Z1 = np.dot(W1,X.T) + b1
+    print(Z1)
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2,A1) + b2
+    A2 = sigmoid(Z2)
+
+    assert(A2.shape == (1, X.shape[0]))
+
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+
+    return A2, cache
+
 def forward_propagation_test_case():
     np.random.seed(1)
-    X_assess = np.random.randn(3, 2)
+    X_assess = np.random.randn(3,2)
 
     parameters = {'W1': np.array([[-0.00416758, -0.00056267],
         [-0.02136196,  0.01640271],
