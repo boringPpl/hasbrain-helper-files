784e679b7878201f6cf9b9dc4e9da42f3b1279f7
chuonghuynh <minhchuong.itus@gmail.com>
2018-12-30 11:30:15 +0700

    Compiled code

--- neural-network/final.py
+++ neural-network/final.py
@@ -61,3 +61,27 @@ def backward_propagation(parameters, cache, X, Y):
              "db2": db2}
 
     return grads
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
+    
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
+
+    W1 = W1 - learning_rate*dW1
+    b1 = b1 - learning_rate*db1
+    W2 = W2 - learning_rate*dW2
+    b2 = b2 - learning_rate*db2
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
